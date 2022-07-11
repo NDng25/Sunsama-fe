90721b79b11595bc94292d5c4b0e2e7e38800bbf
Đoàn Ngọc Châu Anh <doanngocchauanh@gmail.com>
2022-07-11 15:11:10 +0700

    [FE-6] Add Task

--- Frontend/src/components/Column/Column.js
+++ Frontend/src/components/Column/Column.js
@@ -16,7 +16,7 @@ const Column = (props) => {
     },[isShowAddNewTask])
     function FormatDateToAdd(dateTask) {
         let date = dateTask.getDate();
-        let month = dateTask.getMonth();;
+        let month = dateTask.getMonth()+1;
         let year = dateTask.getFullYear();
         if(date<10) date = '0'+date;
         if(month<10) month = '0'+month;
